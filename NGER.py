6e2199ec06afbbec68d458318f72cfc8173e4d2f
DeekshaAsthana <119302730+DeekshaAsthana@users.noreply.github.com>
2023-04-13 23:56:10 +0530

    Update NGER.py

---
+++ NGER.py
@@ -0,0 +1,14 @@
+def nextLargerElement(arr,n):
+        st=[-1]
+        ans=[]
+        for i in range(n-1,-1,-1):
+            if(st[-1]==-1 or st[-1]>arr[i]):
+                ans.append(st[-1])
+                st.append(arr[i])
+            else:
+                while(st[-1]!=-1 and st[-1]<=arr[i]):
+                    st.pop()
+                ans.append(st[-1])
+                st.append(arr[i])
+        ans1=ans[::-1]
+        return ans1
